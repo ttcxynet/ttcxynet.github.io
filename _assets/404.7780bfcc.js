86c9073fb1a8b742e68c0c712c39be9af1a4921f
OneFew <38550226+one-few@users.noreply.github.com>
2021-12-23 19:55:04 +0800

    a
    a

---
+++ _assets/404.7780bfcc.js
@@ -0,0 +1,1 @@
+let e=document.createElement("style");e.innerHTML="body{overflow-y:scroll}",document.head.appendChild(e);import{o as n,c as o}from"./index.5e95bf68.js";var r={name:"error",data:()=>({}),components:{},methods:{},mounted(){}};r.render=function(e,r,t,d,a,l){return n(),o("div",null,"404")};export default r;
