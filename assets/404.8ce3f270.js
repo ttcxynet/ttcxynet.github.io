e107bf48c0f02c76c65cfeda283372675f2b05d4
OneFew <38550226+one-few@users.noreply.github.com>
2022-01-06 22:56:48 +0800

    a
    a

---
+++ assets/404.8ce3f270.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.06e84b55.js";import{o as r,f as o}from"./vendor.3451bb13.js";const t={name:"error",data(){return{}},components:{},methods:{},mounted(){}};function n(_,a,s,c,d,p){return r(),o("div",null,"404")}var f=e(t,[["render",n]]);export{f as default};
